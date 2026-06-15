1073116f3463910553414c01e49bae0fe4736454
Omar <odsantillan@gmail.com>
2026-06-14 21:38:58 -0500

    Creacion nueva rama

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://cdmudqdxrrbuqpywmius.supabase.co/',
+  supabaseUrl: 'https://cdmudqdxrrbuqpywmius.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbXVkcWR4cnJidXFweXdtaXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMjA1MzMsImV4cCI6MjA5Njc5NjUzM30.hxOcI_YrJe6WC-qqoYsCJVfPV5LlS-zHU3fO_gFa118'
-};
+}
